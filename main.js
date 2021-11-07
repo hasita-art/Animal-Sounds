45f9bcf98d0368b247cf4edbe0a489de2ffc9b4c
hasita-art <73806182+hasita-art@users.noreply.github.com>
2021-11-07 13:37:19 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/l6DcVTkFX/model.json",modelReady);
+}
+
+function modelReady(){
+    classifier.classify(gotResults());
+}
+
+function gotResults(error,result){
+    if(error){
+        console.error(error);
+    }
+}
